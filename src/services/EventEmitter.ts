990305e1ffd313f248d2b82677c5858f31b80d06
intelligentRaji <sergdim1234@mail.ru>
2023-06-30 21:37:22 +0200

    feat: update

---
+++ src/services/EventEmitter.ts
@@ -0,0 +1,38 @@
+class EventEmitter<Callback extends (...args: Parameters<Callback>) => void> {
+  private events: Record<string, Callback[]> = {};
+
+  public on(event: string, callback: Callback): void {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
+
+  public off(event: string, callback: Callback): void {
+    const targetEvent = this.events[event];
+
+    if (!targetEvent) {
+      return;
+    }
+
+    targetEvent.filter((item) => item !== callback);
+
+    if (!targetEvent.length) {
+      delete this.events[event];
+    }
+  }
+
+  public emit(event: string, ...args: unknown[]): void {
+    const callableEvent = this.events[event];
+
+    if (!callableEvent) {
+      return;
+    }
+
+    callableEvent.forEach((callback) => {
+      callback(...(args as Parameters<Callback>));
+    });
+  }
+}
+
+export const eventEmitter = new EventEmitter();
